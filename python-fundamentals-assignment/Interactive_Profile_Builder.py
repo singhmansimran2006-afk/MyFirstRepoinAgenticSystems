f5ae5c125d7a16212dbdb1b6797afbabfb6abc1f
Mansimran Singh <singhmansimran2006@gmail.com>
2026-01-30 14:53:15 +0530

    Interactive Profile Builder

---
+++ python-fundamentals-assignment/Interactive_Profile_Builder.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+active_user = bool(input("Are you an active user? True/False: "))
+
+print(f'User {name} is {age} years old. Active Status: {active_user}')
