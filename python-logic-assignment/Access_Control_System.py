61aca956dd18a076a3b4eadd23200d4bf7899e96
Mansimran Singh <singhmansimran2006@gmail.com>
2026-01-30 15:26:44 +0530

    Access Control System

--- python-logic-assignment/Access_Control_System.py
+++ python-logic-assignment/Access_Control_System.py
@@ -1,5 +1,7 @@
 age = int(input("Enter your Age: "))
-has_id = bool(input("Do you have ID? True/False: "))
+has_id = (input("Do you have ID? True/False: "))
+
+has_id = has_id == "True"
 
 if age >= 18 and has_id:
     print("Entry Allowed")
